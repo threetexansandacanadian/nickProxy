ec350f70c5ae859a59df6ce8524c66e1b3f735f8
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-06 13:04:47 -0500

    Changed some stuff

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(morgan('dev'));
+app.use(express.static('dist'));
+
+app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
